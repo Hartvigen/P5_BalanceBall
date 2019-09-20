58db730d57cc6e3e5d6ad548070554e91a74e1ba
Oli Freysson <oleyyh@hotmail.com>
2019-09-20 10:56:59 +0200

    NXTcam testing

--- src/Sean/SeanTest.cpp
+++ src/Sean/SeanTest.cpp
@@ -1,5 +1,4 @@
 #include "SeanTest.h"
-
 int runSean(void)
 {
     /* set pin 5 of PORTB for output*/
@@ -10,39 +9,49 @@ int runSean(void)
 
 void runNX()
 {
-    Serial.begin(9600);
-        delay(2000);   
-
-  Serial.print("Starting...\n");
-   
+    Serial.begin(9600);  // start serial for output
+    delay(500); // wait, allowing time to activate the serial monitor
+    int nblobs = 0;  // Number of blobs
+    uint8_t bc[8], bl[8], bt[8], br[8], bb[8];
+    int x_centre, x_error;
+    int y_centre, y_error;
+    bool erased = false;
+    int i, sq, n, width, height;
     NXShield nxshield;
-
-        Pixy pixy;
-
     nxshield.init(SH_HardwareI2C);
 
-pixy.init();
+    
+    NXTCam nxcam;
 
-   
-  uint32_t i=0;
-   uint8_t r, g, b;
-   
-   while(1)
-   {
-     // calculate r, g, b such that it cycles through the colors
-     r = i&0xff;
-     g = (i*3)&0xff;
-     b = (i/3)&0xff;
-     pixy.setLED(r, g, b);
-     // We need to delay here because serial requests are handled 
-     // every frame period (20ms). If we don't delay, we'll
-     // overrun Pixy's receive queue. But that's all OK because 
-     // we normally only update the LED once per frame anyway.
-     delay(20);
-     
-     i++;
-   }
+    //Initilize camera with the nxshield, and port Bank A Sensor 1.
+    nxcam.init(&nxshield, SH_BAS1);
+
+    //Ved ikk om det gør noget
+    nxcam.start(SH_BAS1);
+
+    Serial.println ("Rdy");
+    //
 
 
-   
+        while (1)
+        {
+            Serial.println(nblobs);
+
+            // Get the blobs from the camera into the array
+            nxcam.getBlobs(&nblobs, bc, bl, bt, br, bb);
+
+
+            // Find the centre of the blob using double resolution of camera
+            x_centre = bl[0] + br[0];
+            y_centre = bt[0] + bb[0];
+
+            // Compute the error from the desired position of the blob (using double resolution)
+            x_error = 176 - x_centre;
+            y_error = 143 - y_centre;
+
+            Serial.println(nblobs);
+            Serial.println(x_centre);
+            Serial.println(y_centre);
+        
+    }
 }
--- src/Sean/SeanTest.h
+++ src/Sean/SeanTest.h
@@ -4,8 +4,7 @@
 #include <Wire.h>
 #include <NXShield.h>
 
-#include <SPI.h>
-#include <Pixy.h>
+#include <NXTCam.h>
 
 #include <avr/io.h> // header file file for input output pins
 #include <util/delay.h> // header file for delay.
