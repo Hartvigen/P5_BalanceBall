3f37971dcdeccfbfc8724c9b142be1c9b0f2dabb
Emil Palmelund Voldby <evoldb17@student.aau.dk>
2019-10-18 15:07:55 +0200

    Attempted debug !_! DOES NOT WORK !_!
    Current attempt at debugging the tracker.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "Camera.h"
 
 #define SLAVE_PIN 53
-#define SKIP_COUNT 4
+#define SKIpointCountOUNT 4
 #define LEFT_BAR 30
 #define RIGHT_BAR 30
 #define CAPTURE_RATE 250
@@ -19,14 +19,26 @@ typedef struct {
 ArduCAM myCAM(OV2640, SLAVE_PIN);
 float Rmin = 31, Gmin = 63, Bmin = 31;
 
+//Needed for main 
 void initCam();
 void calibrateCam();
+byte* calibrateFromImageRow();
 void getBallLocation(int16_t& xCo, int16_t& yCo);
 void readImage(int16_t& xCo, int16_t& yCo);
-void readImageRow(uint16_t& P_c, int16_t& xCo, int16_t& yCo, uint16_t  rowNumber);
+void readImageRow(uint16_t& pointCount, int16_t& xCo, int16_t& yCo, uint16_t  rowNumber);
 void skipImageRows();
 void skipImageRow();
-void calibrateFromImageRow();
+
+//Necessary for Processing
+void takeAndWritePicture();
+void readAndSendImage(ArduCAM myCAM);
+byte* readImageRow(ArduCAM myCAM);
+void skipImageRow(ArduCAM myCAM);
+void writeSerial(byte code, byte* bytes, uint32_t length);
+Packet readSerial();
+byte* intToBytes(uint32_t value);
+byte* int16ToBytes(int16_t value);
+
 
 const uint32_t imageWidth = 320;
 const uint32_t imageHeight = 240;
@@ -50,17 +62,38 @@ int main()
     delay(1000);
 
     initCam();
-    calibrateCam();
     while(Serial.available() == 0){}
     Serial.read();
+    calibrateCam();
     int16_t xCo, yCo;
     uint64_t nextCapture =  0;
     while (true)
     {
-        while(millis() < nextCapture){}
-        nextCapture = millis() + CAPTURE_RATE;
+        digitalWrite(LED_BUILTIN, HIGH);
+        while (!Serial.available()) { delay(2); }
+        Serial.read();
+        digitalWrite(LED_BUILTIN, LOW);
+
+        takeAndWritePicture();
+
+        while (Serial.available()) Serial.read();
+        //while(millis() < nextCapture){}
+        //nextCapture = millis() + CAPTURE_RATE;
         xCo = 0, yCo = 0;
         getBallLocation(xCo, yCo);
+        byte* xbyte = int16ToBytes(xCo + 1);
+        byte* ybyte = int16ToBytes(yCo + 1);
+
+
+        writeSerial(1, xbyte, 2);
+        while (!Serial.available()) { delay(2); }
+        Serial.read();
+        writeSerial(1, ybyte, 2);
+        while (!Serial.available()) { delay(2); }
+        Serial.read();
+
+        delete xbyte;
+        delete ybyte;
         //getBallLocation
         //Choose action (center of ball, current tilt, velocity of ball)
         //perform action (Set tilt)
@@ -98,7 +131,11 @@ void calibrateCam(){
     SPI.transfer(0x00); // The first bit is a header? DON'T remove this. without this, colors goes green goo negative, n' shit.
     for(uint32_t i = 0; i < imageHeight; i++)
     {
-        calibrateFromImageRow();
+        byte* row = calibrateFromImageRow();;
+        writeSerial(0x01, row, imageWidth*3);
+        while(!Serial.available()) {}
+        Serial.read();
+        delete row;
     }
     
     SPI.endTransaction();
@@ -120,22 +157,20 @@ void readImage(int16_t& xCo, int16_t& yCo)
     SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
     myCAM.CS_LOW();
     myCAM.set_fifo_burst();
-    uint16_t P_c = 0;
+    uint16_t pointCount = 0;
     SPI.transfer(0x00); // The first bit is a header? DON'T remove this. without this, colors goes green goo negative, n' shit.
     skipImageRow();
     for(uint16_t i = 0; i < imageHeight-1; i++)
     {
         skipImageRows();
-        readImageRow(P_c, xCo, yCo, i);
-        while(!Serial.available()) {}
-        Serial.read();
+        readImageRow(pointCount, xCo, yCo, i);
     }
     
     SPI.endTransaction();
     myCAM.CS_HIGH();
 }
 
-void readImageRow(uint16_t& P_c, int16_t& xCo, int16_t& yCo,  uint16_t  rowNumber)
+void readImageRow(uint16_t& pointCount, int16_t& xCo, int16_t& yCo,  uint16_t  rowNumber)
 {
     for (uint16_t i = 0; i < imageWidth; i++)
     {
@@ -146,34 +181,41 @@ void readImageRow(uint16_t& P_c, int16_t& xCo, int16_t& yCo,  uint16_t  rowNumbe
         uint32_t c565 = b1 | b2 << 8;
 
         if((c565 & 0x1f) < Rmin && ((c565 >> 5) & 0x3f) < Gmin && ((c565 >> 11) & 0x1f) < Bmin){
-            P_c += 1;
-            xCo += ((i - ceil((float) actualImageWidth/2)) - xCo) / P_c;
-            yCo += (rowNumber - ceil((float) actualImageHeight/2) - yCo)/P_c;
+            pointCount += 1;
+            xCo += (int16_t)((i - ceil((float) actualImageWidth/2)) - xCo) / pointCount;
+            yCo += (int16_t)(rowNumber - ceil((float) actualImageHeight/2) - yCo)/pointCount;
         }
     }
 }
 
-void calibrateFromImageRow()
+byte* calibrateFromImageRow()
 {
-    for (uint32_t i = LEFT_BAR; i < imageWidth-RIGHT_BAR; i++)
+    byte* row = (byte*)malloc(imageWidth*3);
+
+    for (uint32_t i = 0; i < imageWidth; i++)
     {
         byte b1 = SPI.transfer(0x00);
         delayMicroseconds(1);
         byte b2 = SPI.transfer(0x00);
+        if(i < LEFT_BAR || i > imageWidth-RIGHT_BAR)
+            continue;
 
         uint32_t c565 = b1 | b2 << 8;
 
-        Rmin = min(Rmin, (c565 & 0x1f));
-            
+        Rmin = min(Rmin, (c565 & 0x1f));   
         Gmin = min(Gmin, ((c565 >> 5) & 0x3f));
         Bmin = min(Bmin,  ((c565 >> 11) & 0x1f));
         
+        row[3*i+2] = (c565 & 0x1f);
+        row[3*i+1] = ((c565 >> 5) & 0x3f);
+        row[3*i]   = ((c565 >> 11) & 0x1f);
     }
+    return row;
 }
 
 void skipImageRows()
 {
-    uint32_t skipcount = SKIP_COUNT * imageWidth;
+    uint32_t skipcount = SKIpointCountOUNT * imageWidth;
     for (uint32_t i = 0; i < skipcount; i++)
     {
         SPI.transfer(0x00);
@@ -188,4 +230,117 @@ void skipImageRow()
         SPI.transfer(0x00);
         SPI.transfer(0x00);
     }
+}
+
+void takeAndWritePicture()
+{
+    myCAM.flush_fifo();
+    myCAM.start_capture();
+    while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK)) { }
+
+    readAndSendImage(myCAM);
+}
+
+void readAndSendImage(ArduCAM myCAM)
+{
+    SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
+    myCAM.CS_LOW();
+    myCAM.set_fifo_burst();
+
+    SPI.transfer(0x00); // The first bit is a header? DON'T remove this. without this, colors goes green goo negative, n' shit.
+    for(uint32_t i = 0; i < imageHeight; i++)
+    {
+        byte* row = readImageRow(myCAM);
+        writeSerial(0x01, row, imageWidth*3);
+        while(!Serial.available()) {}
+        Serial.read();
+        delete row;
+    }
+    
+    SPI.endTransaction();
+    myCAM.CS_HIGH();
+}
+
+byte* readImageRow(ArduCAM myCAM)
+{
+    byte* row = (byte*)malloc(imageWidth*3);
+
+    for (uint32_t i = 0; i < imageWidth; i++)
+    {
+        byte b1 = SPI.transfer(0x00);
+        delayMicroseconds(1);
+        byte b2 = SPI.transfer(0x00);
+
+        uint32_t c565 = b1 | b2 << 8;
+
+        row[3*i+2] = (c565 & 0x1f);
+        row[3*i+1] = ((c565 >> 5) & 0x3f);
+        row[3*i]   = ((c565 >> 11) & 0x1f);
+    }
+
+    return row;
+}
+
+void skipImageRow(ArduCAM myCAM)
+{
+    for (uint32_t i = 0; i < imageWidth; i++)
+    {
+        SPI.transfer(0x00);
+        SPI.transfer(0x00);
+    }
+}
+
+void writeSerial(byte code, byte* bytes, uint32_t length)
+{
+    byte* size = intToBytes(length);
+    Serial.write(code);
+    Serial.write(size, 4);
+    Serial.write(bytes, length);
+    Serial.flush();
+    delete size;
+}
+
+Packet readSerial()
+{
+    Packet pac;
+
+    byte header[5];
+    while(Serial.available() < 5) { delay(2); }
+    Serial.readBytes(header, 5);
+    
+    pac.code = header[0];
+    pac.size = header[1] | header[2] << 8 | ((uint32_t)header[3]) << 16 | ((uint32_t)header[4]) << 24;
+    
+    uint32_t size = pac.size;
+    pac.data = (byte*)malloc(size);
+
+    while((uint32_t)Serial.available() < (uint32_t)size) { delay(2); }
+    Serial.readBytes(pac.data, size);
+
+    while (Serial.available() > 0)
+        Serial.read();
+
+    return pac;
+}
+
+byte* intToBytes(uint32_t value)
+{
+    byte* buf = (byte*)malloc(4);
+
+    buf[0] = value & 255;
+    buf[1] = (value >> 8)  & 255;
+    buf[2] = (value >> 16) & 255;
+    buf[3] = (value >> 24) & 255;
+
+    return buf;
+}
+
+byte* int16ToBytes(int16_t value)
+{
+    byte* buf = (byte*)malloc(2);
+
+    buf[0] = value & 255;
+    buf[1] = (value >> 8)  & 255;
+
+    return buf;
 }
