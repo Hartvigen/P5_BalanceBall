fd3c35f85a3d699a2fec14d9db834fe593c8d835
Hart <Hartvigen@gmail.com>
2019-10-18 14:36:23 +0200

    Tests work with board attached and comments added for motors

---
+++ test/test_test.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <unity.h>
+
+void test_led_state_high(){
+    digitalWrite(LED_BUILTIN, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(LED_BUILTIN));
+}
+
+void test_led_state_low(){
+    digitalWrite(LED_BUILTIN, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
+}
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();    // IMPORTANT LINE!
+
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+uint8_t i = 0;
+uint8_t max_blinks = 5;
+
+void loop() {
+    int test1 = 20;
+    int test2 = 10;
+
+    test1 += test2;
+
+    TEST_ASSERT_EQUAL(test1, 30);
+
+    if (i < max_blinks)
+    {
+        RUN_TEST(test_led_state_high);
+        delay(500);
+        RUN_TEST(test_led_state_low);
+        delay(500);
+        i++;
+    }
+    else if (i == max_blinks) {
+      UNITY_END(); // stop unit testing
+    }
+}
